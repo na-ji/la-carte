e6c2d25af24c65e9117eea4f2545c5d916e8a983
Naji Astier <naji.demolitionman@gmail.com>
2019-10-28 19:13:39 +0100

    ⚡️ Improve server starting time

--- server/index.ts
+++ server/index.ts
@@ -4,37 +4,58 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 
 import { buildSchema, connectDatabase } from './schema';
+import { GraphQLSchema } from 'graphql';
 
 const isDev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev: isDev });
-const handle = nextApp.getRequestHandler();
+const nextHandler = nextApp.getRequestHandler();
 const port = parseInt(process.env.PORT) || 3000;
+
+function startNextApp(): Promise<void> {
+  console.info('Starting next app');
+
+  return nextApp.prepare();
+}
+
+function connectToDatabase(): Promise<void> {
+  console.info('Connecting to database');
+
+  return connectDatabase();
+}
+
+function buildGQLSchema(): Promise<GraphQLSchema> {
+  console.info('Building GQL schema');
+
+  return buildSchema();
+}
 
 async function bootstrap() {
   try {
-    console.info('Starting next app');
-    await nextApp.prepare();
-
-    console.info('Connecting to database');
-    await connectDatabase();
-
     const server = express();
 
-    console.info('Building GQL schema');
-    const schema = await buildSchema();
+    const nextPromise = startNextApp();
+
+    const [_, schema] = await Promise.all([
+      connectToDatabase(),
+      buildGQLSchema()
+    ]);
 
     console.info('Starting GQL server');
     const apolloServer = new ApolloServer({ schema, tracing: isDev });
     apolloServer.applyMiddleware({ app: server });
 
     server.all('*', (req, res) => {
-      return handle(req, res);
+      return nextHandler(req, res);
     });
 
     console.info('Starting server');
     server.listen(port, err => {
       if (err) throw err;
-      console.log(`> Ready on http://localhost:${port}`);
+      console.log(`> GQL server ready on http://localhost:${port}/graphql`);
+
+      nextPromise.then(() => {
+        console.log(`> Application ready on http://localhost:${port}/`);
+      });
     });
   } catch (err) {
     console.error(err);
